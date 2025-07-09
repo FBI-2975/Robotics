5abf3734bd1bdc5b0e781759628ed1c055ba9e88
FBI-2975 <designbook10000@gmail.com>
2025-07-08 23:22:09 -0600

    include/subsystems.hpp

--- EZ-Template-Example-Project/include/subsystems.hpp
+++ EZ-Template-Example-Project/include/subsystems.hpp
@@ -8,5 +8,5 @@ extern Drive chassis;
 // Your motors, sensors, etc. should go here.  Below are examples
 
 inline pros::Motor halcyon(15); //the number is the port it is on
-inline pros::Motor right(16);
+inline pros::Motor phoenix(16);
 //inline pros::adi::DigitalIn limit_switch('A');
